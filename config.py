165fda30f896c1b8d67684fdda224b615d442360
ERRORQSFG <95165200+ERRORQSFG@users.noreply.github.com>
2021-11-28 12:25:04 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 settings = {
     'CLIENT_ID':'wKqAE18hKwC9qLhKWqflyw',
     'SECRET_CODE':'7tcra6B1G69mbHAqZa2rbAwGxEpaLw',
-    'DISCORD_TOKEN':'OTE0NDIzNzA4OTg1MjAwNjgx.YaM1cQ.kKyKsMz68HddyPoE46KK4Q1Kt0E'
+    'OTE0NDIzNzA4OTg1MjAwNjgx.YaM1cQ.EDjhQ2u2LnpUc1MDAa12nu_uajk'
 }
