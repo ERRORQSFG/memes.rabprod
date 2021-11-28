27e4109a62ab3745f334e6d8c73e192ad4b1819e
ERRORQSFG <95165200+ERRORQSFG@users.noreply.github.com>
2021-11-28 12:23:32 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+settings = {
+    'CLIENT_ID':'wKqAE18hKwC9qLhKWqflyw',
+    'SECRET_CODE':'7tcra6B1G69mbHAqZa2rbAwGxEpaLw',
+    'DISCORD_TOKEN':'OTE0NDIzNzA4OTg1MjAwNjgx.YaM1cQ.kKyKsMz68HddyPoE46KK4Q1Kt0E'
+}
